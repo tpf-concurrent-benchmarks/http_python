eb7ffb12622ef6f604e8143fcd691c9226aae8ee
efoppiano <efoppiano@fi.uba.ar>
2024-04-24 16:03:15 -0300

    Remove unused parameters from hasher.py

--- src/hasher.py
+++ src/hasher.py
@@ -1,13 +1,7 @@
-from dotenv import load_dotenv
-import os
 from passlib.context import CryptContext
-
-load_dotenv()
 
 class Hasher:
     def __init__(self):
-        self.secret_key = os.environ["SECRET_KEY"]
-        self.algorithm = os.environ["ALGORITHM"]
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def hash_password(self, password: str) -> str:
