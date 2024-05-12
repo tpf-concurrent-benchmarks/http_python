169419b1233cc0286f5c7abaedad56781ebcec1b
efoppiano <efoppiano@fi.uba.ar>
2024-05-12 15:18:18 -0300

    Rename PollPreviewSerializer.poll_id to PollPreviewSerializer.id to match specs

--- src/serializers/polls/poll_preview.py
+++ src/serializers/polls/poll_preview.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
+from src.models.polls import PollModel
 
 class PollPreviewSerializer(BaseModel):
-    poll_id: int
+    id: int
     poll_topic: str
 
-    class Config:
-        orm_mode = True
+    @classmethod
+    def from_orm(cls, poll: PollModel):
+        return cls(id=poll.poll_id, poll_topic=poll.poll_topic)
